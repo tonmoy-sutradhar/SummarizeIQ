a01b499ba09daf36daf2b1b7063bf6d181cc32d1
Tonmoy <tonmoysutradhar2002@gmail.com>
2025-05-31 01:29:59 +0600

    Header and footer added done .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Source_Sans_3 as FontSans } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/common/header";
+import Footer from "@/components/common/Footer";
 
 const fontSans = FontSans({
   variable: "--font-sans",
@@ -22,7 +24,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${fontSans.variable} font-sans antialiased`}>
-        {children}
+        <Header></Header>
+        <main>{children}</main>
+        <Footer></Footer>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Hello Tonmoy
+    <div>
+      <h1 className="text-3xl font-bold">SummarizeIQ</h1>
+      <Button variant={"outline"} size={"lg"}>
+        Shadcn UI
+      </Button>
     </div>
   );
 }
+// grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"
