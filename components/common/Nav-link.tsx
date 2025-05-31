a7617354fd7fda7e9792c51d6cc10c5cc2026c71
Tonmoy <tonmoysutradhar2002@gmail.com>
2025-06-01 00:16:49 +0600

    Custome Nav-link add for simply link to other.

---
+++ components/common/Nav-link.tsx
@@ -0,0 +1,24 @@
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+export default function NavLink({
+  href,
+  children,
+  className,
+}: {
+  href: string;
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <Link
+      href={href}
+      className={cn(
+        "transition-colors text-sm duration-200 text-gray-600 hover:text-rose-500",
+        className
+      )}
+    >
+      {children}
+    </Link>
+  );
+}
--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,35 +1,39 @@
 import Link from "next/link";
 import { FileText } from "lucide-react";
 import { Button } from "../ui/button";
+import NavLink from "./Nav-link";
 
 export default function Header() {
   const isLoggedIn = false;
   return (
     <nav className="container flex items-center justify-between py-4 px-2 lg:px-8 mx-auto">
       <div className="flex lg:flex-1">
-        <Link className="flex items-center gap-1 lg:gap-2 shrink-0" href={"/"}>
+        <NavLink
+          className="flex items-center gap-1 lg:gap-2 shrink-0"
+          href={"/"}
+        >
           <FileText className="w-5 h-5 lg:w-8 lg:h-8 text-gray-900 hover:rotate-12 transform transition duration-200 ease-in-out cursor-pointer"></FileText>
           <span className="font-extrabold lg:text-xl text-gray-900">
             SummarizeIQ
           </span>
-        </Link>
+        </NavLink>
       </div>
 
       <div className="flex lg:justify-center gap-4 lg:gap-12 lg:items-center">
-        <Link href={"/#pricing"}>Pricing</Link>
-        {isLoggedIn && <Link href={"/#dashboard"}>Your Summaries</Link>}
+        <NavLink href={"/#pricing"}>Pricing</NavLink>
+        {isLoggedIn && <NavLink href={"/#dashboard"}>Your Summaries</NavLink>}
       </div>
 
       <div className="flex lg:justify-end gap-4 lg:flex-1 lg:items-cente">
         {isLoggedIn ? (
           <div className="flex gap-2 items-center">
-            <Link href={"/#upload"}>Upload a PDF</Link>
+            <NavLink href={"/#upload"}>Upload a PDF</NavLink>
             <div>Pro</div>
             <Button>User</Button>
           </div>
         ) : (
           <div>
-            <Link href={"/#sign-in"}>SignIn</Link>
+            <NavLink href={"/#sign-in"}>SignIn</NavLink>
           </div>
         )}
       </div>
