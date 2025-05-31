d94f390bee16446ba768dcb74921bf1984d9d2d0
Tonmoy <tonmoysutradhar2002@gmail.com>
2025-06-01 00:04:50 +0600

    Navbar added done.

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,16 +1,37 @@
 import Link from "next/link";
+import { FileText } from "lucide-react";
+import { Button } from "../ui/button";
 
 export default function Header() {
+  const isLoggedIn = false;
   return (
-    <nav className="container flex items-center justify-between px-5">
-      <div>
-        <Link href={"/"}>SummarizeIQ</Link>
+    <nav className="container flex items-center justify-between py-4 px-2 lg:px-8 mx-auto">
+      <div className="flex lg:flex-1">
+        <Link className="flex items-center gap-1 lg:gap-2 shrink-0" href={"/"}>
+          <FileText className="w-5 h-5 lg:w-8 lg:h-8 text-gray-900 hover:rotate-12 transform transition duration-200 ease-in-out cursor-pointer"></FileText>
+          <span className="font-extrabold lg:text-xl text-gray-900">
+            SummarizeIQ
+          </span>
+        </Link>
       </div>
-      <div>
-        <Link href={"/#"}>Pricing</Link>
+
+      <div className="flex lg:justify-center gap-4 lg:gap-12 lg:items-center">
+        <Link href={"/#pricing"}>Pricing</Link>
+        {isLoggedIn && <Link href={"/#dashboard"}>Your Summaries</Link>}
       </div>
-      <div>
-        <Link href={"/#"}>SignIn</Link>
+
+      <div className="flex lg:justify-end gap-4 lg:flex-1 lg:items-cente">
+        {isLoggedIn ? (
+          <div className="flex gap-2 items-center">
+            <Link href={"/#upload"}>Upload a PDF</Link>
+            <div>Pro</div>
+            <Button>User</Button>
+          </div>
+        ) : (
+          <div>
+            <Link href={"/#sign-in"}>SignIn</Link>
+          </div>
+        )}
       </div>
     </nav>
   );
