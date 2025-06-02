927773bdfdb774d5a9b556546d1bc92d6ea9a546
Tonmoy <tonmoysutradhar2002@gmail.com>
2025-06-03 00:19:24 +0600

    Signup page done and shadow clor added .

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,3 +1,4 @@
+import BgGradient from "@/components/common/BgGradient";
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
@@ -5,6 +6,7 @@ export default function Page() {
     <section className="flex justify-center items-center lg:min-h-[40vh]">
       {" "}
       <div className="py-12 lg:py-24 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 lg:pt-12">
+        <BgGradient className="from-rose-400 via-rose-300 to-orange-200"></BgGradient>
         <SignIn />{" "}
       </div>
     </section>
