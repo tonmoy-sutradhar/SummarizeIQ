d55e25addb298b035971623e1f7772bad00e8221
Tonmoy <tonmoysutradhar2002@gmail.com>
2025-06-04 23:53:20 +0600

    ZOD implementing for secure file upload.

--- components/upload/UploadForm.tsx
+++ components/upload/UploadForm.tsx
@@ -1,5 +1,19 @@
 "use client";
+import { z } from "zod";
 import UploadFormInput from "./UploadFormInput";
+
+const schema = z.object({
+  file: z
+    .instanceof(File, { message: "Invalid file." })
+    .refine(
+      (file) => file.size <= 20 * 1024 * 1024,
+      "File size must be less than 20MB."
+    )
+    .refine(
+      (file) => file.type.startsWith("application/pdf"),
+      "File must be a PDF."
+    ),
+});
 
 export default function UploadForm() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -9,6 +23,13 @@ export default function UploadForm() {
     const file = formData.get("file") as File;
 
     // validating the fields
+    const validatedFields = schema.safeParse({ file });
+
+    if (!validatedFields.success) {
+      console.log(
+        validatedFields.error.flatten().fieldErrors.file?.[0] ?? "Invalid file"
+      );
+    }
     // schema with zod
     // upload the file to uploadThing
     // parse the pdf using lang chain
