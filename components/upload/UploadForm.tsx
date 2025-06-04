cff6aeee6ede22c96eedc509581be2e19a4a8f41
Tonmoy <tonmoysutradhar2002@gmail.com>
2025-06-04 23:23:13 +0600

    file upload processing done.

--- components/upload/UploadForm.tsx
+++ components/upload/UploadForm.tsx
@@ -2,11 +2,22 @@
 import UploadFormInput from "./UploadFormInput";
 
 export default function UploadForm() {
-  const handleSubmit = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     console.log("Submitted");
+    const formData = new FormData(e.currentTarget);
+    const file = formData.get("file") as File;
+
+    // validating the fields
+    // schema with zod
+    // upload the file to uploadThing
+    // parse the pdf using lang chain
+    // summarize the pdf using AI
+    // Save the summary to the database
+    // redirect to the [id] summary page
   };
   return (
-    <div>
+    <div className="flex flex-col gap-8 w-full max-w-2xl">
       <UploadFormInput onsubmit={handleSubmit}></UploadFormInput>
     </div>
   );
--- components/upload/UploadFormInput.tsx
+++ components/upload/UploadFormInput.tsx
@@ -9,16 +9,18 @@ interface UploadFormInputProps {
 export default function UploadFormInput({ onsubmit }: UploadFormInputProps) {
   return (
     <div>
-      <form action="" className="flex flex-col gap-6">
-        <Input
-          id="file"
-          name="file"
-          accept="application/pdf"
-          type="file"
-          required
-          className=""
-        />
-        <Button>Upload your PDF</Button>
+      <form action="" className="flex flex-col gap-6 onSubmit= {onSubmit}">
+        <div className="flex justify-end items-center gap-1.5">
+          <Input
+            id="file"
+            name="file"
+            accept="application/pdf"
+            type="file"
+            required
+            className=""
+          />
+          <Button>Upload your PDF</Button>
+        </div>
       </form>
     </div>
   );
