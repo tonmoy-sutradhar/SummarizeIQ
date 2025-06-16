183af9aec688abd774c5f463a7ecbf889d260d5b
Tonmoy <tonmoysutradhar2002@gmail.com>
2025-06-16 15:31:17 +0600

    Improve the design specific pdf page.

--- app/(logged-in)/summaries/[id]/page.tsx
+++ app/(logged-in)/summaries/[id]/page.tsx
@@ -51,14 +51,20 @@ export default async function SummaryPage(props: {
     notFound();
   }
 
-  const { title, summary_text, file_name, word_count } = summary;
+  const { title, summary_text, file_name, word_count, created_at } = summary;
+
+  const readigTime = Math.ceil((word_count || 0) / 200);
   return (
     <div className="relative isolate min-h-screen bg-linear-to-b from-blue-50/40 to-white">
       <BgGradient className="from-blue-400 via-blue-300 to-orange-200"></BgGradient>
       <div className="container mx-auto flex flex-col gap-4">
         <div className="px-4 sm:px-6 lg:px-8 py-6 sm:py-12 lg:py-24">
           <div className="flex flex-col">
-            <SummaryHeader title={title}></SummaryHeader>
+            <SummaryHeader
+              title={title}
+              createdAt={created_at}
+              readingTime={readigTime}
+            ></SummaryHeader>
             {file_name && <SourceInfo fileName={file_name}></SourceInfo>}
             <div className="relative mt-4 sm:mt-8 lg:mt-16">
               {/* line */}
@@ -66,7 +72,7 @@ export default async function SummaryPage(props: {
                 <div className="absolute inset-0 bg-linear-to-br from-rose-50/50 via-orange-50/30 to-transparent opacity-50 rounded-2xl sm:rounded-3xl"></div>
 
                 <div className="absolute top-2 sm:top-4 right-2 sm:right-4 flex items-center gap-1.5 sm:gap-2 text-xs sm:text-sm text-muted-foreground bg-white/90 px-2 sm:px-3 py-1 sm:py-1.5 rounded-full shadow">
-                  <FileText className="h-3 w-3 sm:h-4 sm:w-4 text-rose-400" />
+                  <FileText className="h-3 w-3 sm:h-4 sm:w-4 text-blue-400" />
                   {word_count?.toLocaleString()} words
                 </div>
 
--- components/summaries/summaryHeader.tsx
+++ components/summaries/summaryHeader.tsx
@@ -1,20 +1,54 @@
 import Link from "next/link";
 import { Button } from "../ui/button";
-import { ChevronLeft } from "lucide-react";
+import { Calendar, ChevronLeft, Clock, Sparkles } from "lucide-react";
+import { Badge } from "../ui/badge";
 
-export default function SummaryHeader({ title }: { title: string }) {
+export default function SummaryHeader({
+  title,
+  createdAt,
+  readingTime,
+}: {
+  title: string;
+  createdAt: string;
+  readingTime: number;
+}) {
   return (
     <div className="flex gap-4 mb-4 justify-between items-center">
-      <div className="text-4xl font-bold ">{title}</div>
-      <div>
-        {/* <Link href={"/dashboard"}>
-          <Button
-            size={"sm"}
-            className="group flex items-center gap-1 sm:gap-2 hover"
+      <div className="space-y-6 ">
+        <div className="flex flex-wrap items-center gap-4">
+          <Badge
+            variant={"secondary"}
+            className="relative px-4 text-sm font-medium bg-blue-100 text-blue-500 backdrop-blur-xs rounded-full hover:bg-white/90 transition-all duration-200 shadow-xs hover:shadow-md"
           >
-            Back to Dashboard
-          </Button>
-        </Link> */}
+            {/* <Sparkles className="h-4 w-4 mr-1.5"></Sparkles>*/}
+            <Sparkles
+              className="text-blue-600 animate-pulse"
+              style={{ width: "20px", height: "20px" }}
+            />
+            AI Summary
+          </Badge>
+          <div className="flex  items-center gap-2 text-sm text-muted-foreground">
+            <Calendar className="text-blue-400"></Calendar>
+            {new Date(createdAt).toLocaleDateString("en-US", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            })}
+          </div>
+
+          {/* Reading Time */}
+          <div className="flex  items-center gap-2 text-sm text-muted-foreground">
+            <Clock className="text-blue-400"></Clock>
+            {readingTime} min Read
+          </div>
+        </div>
+        <h1 className="text-2xl lg:text-4xl font-bold lg:tracking-tight">
+          <span className="bg-linear-to-r from-blue-600 to-lime-500 bg-clip-text text-transparent">
+            {title}
+          </span>
+        </h1>
+      </div>
+      <div className="self-start">
         <Button
           variant={"link"}
           className=" group rounded-2xl hover:no-underline flex items-center gap-1 sm:gap-3 border-blue-100/30 bg-blue-100 px-2 sm:px-3"
@@ -23,7 +57,10 @@ export default function SummaryHeader({ title }: { title: string }) {
             href={"/dashboard"}
             className="text-blue-500  flex items-center "
           >
-            <ChevronLeft className="h-3 w-3  text-blue-500 sm:h-4 sm:w-4 transition-transform group-hover:translate-x-0.5"></ChevronLeft>
+            <ChevronLeft
+              className=" text-blue-500 sm:h-4 sm:w-4 transition-transform group-hover:translate-x-0.5"
+              style={{ width: "20px", height: "20px" }}
+            ></ChevronLeft>
             {"    "} Back <span className="hidden sm:inline">to Dashboard</span>
           </Link>
         </Button>
