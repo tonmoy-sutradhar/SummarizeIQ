2735c24cfdab8a84c85f887743b505ae1b3c0980
Tonmoy <tonmoysutradhar2002@gmail.com>
2025-06-29 21:45:54 +0600

    Progress bar addede in card section of summaries pdf done.

--- components/summaries/summaryViewer.tsx
+++ components/summaries/summaryViewer.tsx
@@ -3,8 +3,9 @@
 import { useState } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { NavigationControl } from "./NavigationControl";
+import ProgressBar from "./ProgressBar";
 
-const parseSection = (section: string) => {
+const parseSection = (section: string): { title: string; points: string[] } => {
   const [title, ...content] = section.split("\n");
 
   const cleanTitle = title.startsWith("#")
@@ -55,6 +56,13 @@ export function SummaryViewer({ summary }: { summary: string }) {
   return (
     <Card className="relative px-2 h-[300px] sm:h-[400px] lg:h-[400px] w-full xl:w-[600px] overflow-hidden bg-linear-to-br from-background via-background/95 to-blue-500/5 backdrop-blur-lg shadow-2xl rounded-3xl border border-blue-500/10">
       {/* {JSON.stringify(sections[currentSection].points)} */}
+
+      {/* ProgressBar */}
+      <ProgressBar
+        sections={sections}
+        currentSection={currentSection}
+      ></ProgressBar>
+
       <div className="h-full overflow-y-auto scrollbar-hide pt-12 sm:pt-16 pb-20 sm:pb-24">
         <div className="px-4 sm:px-6">
           <h2>{sections[currentSection]?.title || ""}</h2>
